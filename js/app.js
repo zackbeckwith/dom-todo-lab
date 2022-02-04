ae05403212981845e4e22263881c79f4d2077175
zackbeckwith <z.beckwith99@gmail.com>
2022-02-03 19:49:20 -0500

    complete html

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('sanity check')
+const button = document.querySelector('subbutton')
+const input = document.querySelector('#subbox')
+const list = document.querySelector('todo-list')
+
+button.addEventListener('click', function(){
+  console.log('test')
+})
+
+
