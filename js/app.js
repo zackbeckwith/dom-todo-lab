b0cc84307f67ed2b1830cf08774560b9ae6ea514
zackbeckwith <z.beckwith99@gmail.com>
2022-02-03 20:20:18 -0500

    add ability to reset list

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
 const button = document.querySelector('#subbutton')
 const input = document.querySelector('#subbox')
 const list = document.querySelector('#todo-list')
-
+const reset = document.querySelector('#reset')
 button.addEventListener('click', function(){
   const newToDo = document.createElement('li')
-
   newToDo.textContent = input.value
   if(input.value !== "") {
     document.querySelector('ul').appendChild(newToDo)
+  }
+  input.value = ""
+})
+
+reset.addEventListener('click', function(){
+  while (list.firstChild) {
+    list.removeChild(list.firstChild)
   }
   input.value = ""
 })
